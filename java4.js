ef6858f3c4beb26299a45a7dc90e0962ef99d47d
Nipun0724 <132289519+Nipun0724@users.noreply.github.com>
2023-07-09 20:11:03 +0530

    Add files via upload

---
+++ java4.js
@@ -0,0 +1,98 @@
+let sum = 0;
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+let count4 = 0;
+let count5 = 0;
+let count6 = 0;
+let count7 = 0;
+let count8 = 0;
+let count9 = 0;
+let count10 = 0;
+let count11 = 0;
+let count12 = 0;
+document.getElementById("total").textContent = "Total : 0";
+function add1() {
+  count1 += 1;
+  let price1 = document.getElementById("itemprice1").textContent;
+  sum += Number(price1);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count1").textContent = count1;
+}
+function add2() {
+  count2 += 1;
+  let price2 = document.getElementById("itemprice2").textContent;
+  sum += Number(price2);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count2").textContent = count2;
+}
+function add3() {
+  count3 += 1;
+  let price3 = document.getElementById("itemprice3").textContent;
+  sum += Number(price3);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count3").textContent = count3;
+}
+function add4() {
+  count4 += 1;
+  let price4 = document.getElementById("itemprice4").textContent;
+  sum += Number(price4);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count4").textContent = count4;
+}
+function add5() {
+  count5 += 1;
+  let price5 = document.getElementById("itemprice5").textContent;
+  sum += Number(price5);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count5").textContent = count5;
+}
+function add6() {
+  count6 += 1;
+  let price6 = document.getElementById("itemprice6").textContent;
+  sum += Number(price6);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count6").textContent = count6;
+}
+function add7() {
+  count7 += 1;
+  let price7 = document.getElementById("itemprice7").textContent;
+  sum += Number(price7);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count7").textContent = count7;
+}
+function add8() {
+  count8 += 1;
+  let price8 = document.getElementById("itemprice8").textContent;
+  sum += Number(price8);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count8").textContent = count8;
+}
+function add9() {
+  count9 += 1;
+  let price9 = document.getElementById("itemprice9").textContent;
+  sum += Number(price9);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count9").textContent = count9;
+}
+function add10() {
+  count10 += 1;
+  let price10 = document.getElementById("itemprice10").textContent;
+  sum += Number(price10);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count10").textContent = count10;
+}
+function add11() {
+  count11 += 1;
+  let price11 = document.getElementById("itemprice11").textContent;
+  sum += Number(price11);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count11").textContent = count11;
+}
+function add12() {
+  count12 += 1;
+  let price12 = document.getElementById("itemprice12").textContent;
+  sum += Number(price12);
+  document.getElementById("total").textContent = "Total : " + sum;
+  document.getElementById("count12").textContent = count12;
+}
